93ccb93cee1175c270fa7100ec9210e240a7e629
Marc Majcher <majcher@gmail.com>
2017-01-24 15:01:25 -0600

    simple chat

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-'ust strict';
+'use strict';
 
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function() {
+io.on('connection', (socket) => {
+  socket.broadcast.emit('* connected *');
+  socket.on('chat', (msg) => {
+    io.emit('chat', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('DIIIIISCONNETED');
+  });
+});
+
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
